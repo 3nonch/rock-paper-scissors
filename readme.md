588c555d95c47981a5d8e8cdff69236555e3cf0d
Inanc Ciftci <115939077+3nonch@users.noreply.github.com>
2022-12-14 11:19:30 +0300

    Add files via upload
    -initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+## The first javascript game I've coded alone.
+
+-This is a simple best of 5 rock paper scissors game coded with javaScript.
+\*There's a lot of bugs that i'm aware of. I will try to fix them as i learn more.
---
+++ script.js
@@ -0,0 +1,104 @@
+const pScoreBox = document.querySelector(".pScore");
+const cScoreBox = document.querySelector(".cScore");
+
+const winnerBox = document.querySelector(".winner");
+
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+const resetBtn = document.querySelector(".reset");
+
+const playerImg = document.querySelector(".playerOneImg");
+const compImg = document.querySelector(".playerTwoImg");
+
+let playerScore = 0;
+let computerScore = 0;
+
+const getComputerChoice = function () {
+  const compChoiceArr = ["rock", "paper", "scissors"];
+  const getRandomChoice =
+    compChoiceArr[Math.floor(Math.random() * compChoiceArr.length)];
+  return getRandomChoice;
+};
+
+const determineWinner = function (player, computer) {
+  if (player === computer) {
+    return (winnerBox.textContent = `tie 🤝`);
+  }
+  if (player === "rock") {
+    if (computer === "paper") {
+      computerScore++;
+      return (winnerBox.textContent = `computer 💻`);
+    } else if (computer === "scissors") {
+      playerScore++;
+      return (winnerBox.textContent = `player 🥳`);
+    }
+  }
+  if (player === "paper") {
+    if (computer === "rock") {
+      playerScore++;
+      return (winnerBox.textContent = `player 🥳`);
+    } else if (computer === "scissors") {
+      computerScore++;
+      return (winnerBox.textContent = `computer 💻`);
+    }
+  }
+  if (player === "scissors") {
+    if (computer === "rock") {
+      computerScore++;
+      return (winnerBox.textContent = `computer 💻`);
+    } else if (computer === "paper") {
+      playerScore++;
+      return (winnerBox.textContent = `player 🥳`);
+    }
+  }
+};
+
+const playRound = function (player, computer) {
+  determineWinner(player, computer);
+  pScoreBox.textContent = playerScore;
+  cScoreBox.textContent = computerScore;
+};
+
+rockBtn.addEventListener("click", function () {
+  player = "rock";
+  computer = getComputerChoice();
+  playerImg.src = "./rock.png";
+  compImg.src = `./${computer}.png`;
+  if (playerScore < 3 && computerScore < 3) {
+    playRound(player, computer);
+  } else {
+    alert(`This round is over, please reset the game.`);
+  }
+});
+
+paperBtn.addEventListener("click", function () {
+  player = "paper";
+  computer = getComputerChoice();
+  playerImg.src = "./paper.png";
+  compImg.src = `./${computer}.png`;
+  if (playerScore < 3 && computerScore < 3) {
+    playRound(player, computer);
+  } else {
+    alert(`This round is over, please reset the game.`);
+  }
+});
+
+scissorsBtn.addEventListener("click", function () {
+  player = "scissors";
+  computer = getComputerChoice();
+  playerImg.src = "./scissors.png";
+  compImg.src = `./${computer}.png`;
+  if (playerScore < 3 && computerScore < 3) {
+    playRound(player, computer);
+  } else {
+    alert(`This round is over, please reset the game.`);
+  }
+});
+
+resetBtn.addEventListener("click", function () {
+  playerScore = 0;
+  computerScore = 0;
+  pScoreBox.textContent = "0";
+  cScoreBox.textContent = "0";
+});
